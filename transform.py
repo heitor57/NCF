a0dc6c76ddab013020a4fd086feef68f48502955
heitor57 <heitorwerneck@hotmail.com>
2021-04-29 21:01:05 -0300

    Transform GMF

--- transform.py
+++ transform.py
@@ -19,6 +19,18 @@ import evaluate
 import data_utils
 model = torch.load('{}{}_{}.pth'.format(config.model_path, config.model,config.dataset))
 
-np.array(model.state_dict()['embed_user_GMF.weight'])
-np.array(model.state_dict()['embed_item_GMF.weight'])
+with open('user_feature.npy', 'wb') as f:
+	# np.save(f,np.array(model.state_dict()['embed_user_GMF.weight']))
+	# print(np.array(model.state_dict()['embed_user_GMF.weight']).shape)
+	# print(np.array([[0]*256]).shape)
+	# a = np.vstack((np.array([[0]*256]),np.array(model.state_dict()['embed_user_GMF.weight'])))
+	a = np.array(model.state_dict()['embed_user_GMF.weight'])
+	a=a.astype('float32')
+	np.save(f,a)
+with open('item_feature.npy', 'wb') as f:
+	a = np.array(model.state_dict()['embed_item_GMF.weight'])
+	a=a.astype('float32')
+	np.save(f,a)
+
+# np.array(model.state_dict()['embed_item_GMF.weight'])
 # print("End. Best epoch {:03d}: HR = {:.3f}, NDCG = {:.3f}".format(
